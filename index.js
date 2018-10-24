f0a058f4128037e4e766d143ddc2a930f8e7bdf9
Gabriel Cardona <gabriel@bitcoin.com>
2018-10-24 07:27:05 -0700

    v

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const repl = require("repl");
 const SLP = require("./lib/SLP").default;
 const clone = require("git-clone");
 
-program.version("0.0.6", "-v, --version");
+program.version("0.0.7", "-v, --version");
 
 program
   .command("new <name>")
