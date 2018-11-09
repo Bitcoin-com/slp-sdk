50b17d7869eb53e16bc02d7ed79855a27cb5fc81
Gabriel Cardona <gabriel@bitcoin.com>
2018-11-10 04:48:06 +0900

    Port Conversion class to .ts.

--- src/Conversion.ts
+++ src/Conversion.ts
@@ -1,18 +1,18 @@
 const BITBOXSDK = require("bitbox-sdk/lib/bitbox-sdk").default
-let BITBOX = new BITBOXSDK()
-let utils = require("slpjs").utils
+const BITBOX = new BITBOXSDK()
+const utils = require("slpjs").utils
 
 class Conversion {
-  toSLPAddress(address) {
+  toSLPAddress(address: string): string {
     return utils.toSlpAddress(address)
   }
 
-  toCashAddress(address) {
+  toCashAddress(address: string): string {
     return utils.toCashAddress(address)
   }
 
-  toLegacyAddress(address) {
-    let cashAddr = utils.toCashAddress(address)
+  toLegacyAddress(address: string): string {
+    const cashAddr = utils.toCashAddress(address)
     return BITBOX.Address.toLegacyAddress(cashAddr)
   }
 }
--- src/List.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-class List {
-  constructor(restURL) {
-    this.restURL = restURL
-  }
-}
-
-export default List
