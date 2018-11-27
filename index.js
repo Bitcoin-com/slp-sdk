72acf2ac2f9e1bd577036fd66114cee9f88f5f02
Gabriel Cardona <gabriel@bitcoin.com>
2018-11-27 19:44:07 +0800

    Remove nodemon dep and bump BITBOX version to address event-stream vuln.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const repl = require("repl")
 const SLP = require("./lib/SLP").default
 const clone = require("git-clone")
 
-program.version("0.4.9", "-v, --version")
+program.version("0.4.10", "-v, --version")
 
 program
   .command("new <name>")
