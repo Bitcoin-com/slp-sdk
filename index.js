b197fd8e72297e86fbfdc87e332b49a3a3d3f245
Gabriel Cardona <gabriel@bitcoin.com>
2019-05-05 06:54:28 +0900

    feat(bump-v) MINOR bump v

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const repl = require("repl")
 const SLP = require("./lib/SLP")
 const clone = require("git-clone")
 
-program.version("3.6.0", "-v, --version")
+program.version("3.7.0", "-v, --version")
 
 program
   .command("new <name>")
