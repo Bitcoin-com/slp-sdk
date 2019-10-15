fde268be7b4dee9bcaa5b2d7a929c4288f54f30b
Chris Troutner <chris.troutner@gmail.com>
2019-10-15 08:57:35 -0700

    Adding additional error handling to TokenType1.send()

--- src/TokenType1.ts
+++ src/TokenType1.ts
@@ -157,6 +157,10 @@ class TokenType1 {
       }
 
       let inputUtxos = balances.slpTokenUtxos[tokenId]
+      // console.log(`inputUtxos: ${JSON.stringify(inputUtxos, null, 2)}`)
+      // console.log(`balances.nonSlpUtxos: ${JSON.stringify(balances.nonSlpUtxos, null, 2)}`)
+
+      if(inputUtxos === undefined) throw new Error(`Could not find any non-SLP UTXOs`)
 
       inputUtxos = inputUtxos.concat(balances.nonSlpUtxos)
 
