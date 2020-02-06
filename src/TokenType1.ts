f2514f4e50538c7c888d0b9996bd390a9b22e100
josephroyking <joey@bitcoin.com>
2020-02-06 10:58:43 +0100

    Same issue for multiple funding addresses

--- src/TokenType1.ts
+++ src/TokenType1.ts
@@ -298,8 +298,12 @@ class TokenType1 {
     // bchChangeReceiverAddress can be either simpleledger or cashAddr format
     // validate fundingAddress format
     // single fundingAddress
-    if (config.fundingAddress && !addy.isSLPAddress(config.fundingAddress))
-      throw Error("Funding Address must be simpleledger format");
+
+    if (config.fundingAddress && !Array.isArray(config.fundingAddress)) {
+      if (!addy.isSLPAddress(config.fundingAddress)) {
+        throw Error("Token Receiver Address must be simpleledger format");
+      }
+    }
 
     // bulk fundingAddress
     if (config.fundingAddress && Array.isArray(config.fundingAddress)) {
